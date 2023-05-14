70c56da371d9c7e4b69dc981e6cd428509d98843
alexander22412221 <90434609+alexanderr4@users.noreply.github.com>
2023-05-13 19:34:52 -0500

    Update front-app.service.ts

--- src/app/front-app/front-app.service.ts
+++ src/app/front-app/front-app.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FrontAppService {
 
-  private apiUrl = 'http://10.4.21.133:80/api/machine-info';
+  private apiUrl = 'http://192.168.1.4:80/api/machine-info';
 
   constructor(private http: HttpClient) { }
 
